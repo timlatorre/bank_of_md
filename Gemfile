d43fa37db68293225af81472dba22490e0d83651
Tim LaTorre <timlatorre@gmail.com>
2012-12-17 15:06:31 -0500

    init

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'jquery-rails'
+gem 'pg'
+gem 'bcrypt-ruby'
+
+group :development do
+  gem 'pry-rails'
+  gem 'annotate'
+  gem 'quiet_assets'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails'
+  gem 'zurb-foundation'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankOfMd::Application.config.secret_token = '84102a645fe674a73c20e91222f1fb93377bd599354ae7469aed8e6ba177908bd8f60d32abb37a69c03ff24e8986bf92c84f28a289022048433904ba65b41e98'
