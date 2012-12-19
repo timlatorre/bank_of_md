63f1e9068e5adabba706838b4d3f43a5e49bb319
Tim LaTorre <timlatorre@gmail.com>
2012-12-19 09:34:14 -0500

    version 1 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
   def display_links
     if @auth
-      link_to("Overview", overview_path) +
+      link_to("Logout", logout_path, :method => :delete) +
+      link_to("Family", profile_path) +
       link_to("Chores", chores_path) +
-      link_to("New Transaction", transactions_path) +
-      link_to("Logout", logout_path, :method => :delete)
+      link_to("Transactions", transactions_path) +
+      link_to("Overview", overview_path)
     else
-      link_to("Register", register_path) +
-      link_to("Login", login_path)
+      link_to("Login", login_path) +
+      link_to("Register", register_path)
     end
   end
 end
